00c4c39404abdaddca1188cd7d2186587ba2076e
刘楠 <liunan.coulson@bytedance.com>
2018-07-20 12:25:00 +0800

    cpu recorder

--- Example/MNMonitor/MNViewController.m
+++ Example/MNMonitor/MNViewController.m
@@ -8,9 +8,11 @@
 
 #import "MNViewController.h"
 #import "MNMonitorTrackerConfig.h"
+#import "MNCPURecorder.h"
 
 @interface MNViewController ()
 @property (nonatomic, strong) MNMonitorTrackerConfig *config;
+@property (nonatomic, strong) MNCPURecorder *recorder;
 @end
 
 @implementation MNViewController
@@ -26,7 +28,11 @@
     }] runloopConfig:[MNMainRunloopTrackerConfig instanceWithTimeInterval:5.0 completionBlock:^(double currentUsage, BOOL isSucceed) {
         
     }]];
-    NSLog(@"b");
+    _recorder = [MNCPURecorder new];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        double useage = [self.recorder currentCPUUsage];
+        NSLog(@"cpu usage: %f",useage);
+    }];
 }
 
 - (void)didReceiveMemoryWarning
